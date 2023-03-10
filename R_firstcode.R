436a6651e06a618bb206c5a307b9d446d05cf400
leonardofarinatti <127427288+leonardofarinatti@users.noreply.github.com>
2023-03-10 17:45:46 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -4,3 +4,17 @@
 install.packages("raster")
 
 library(raster)
+
+# Import data, setting the working directory
+setwd("C:/Lab/")
+
+l2011 <- brick("p224r63_2011_masked.grd")
+
+# plotting the data in a savage manner
+plot(l2011)
+
+cl <- colorRampPalette(c("red", "orange", "yellow")) (100)
+plot(l2011, col=cl) # per assegnare i nuovi colori alle immagini
+
+#plotting one element
+plot(l2011[[4]], col=cl)
