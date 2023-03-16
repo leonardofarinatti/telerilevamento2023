ce70d6ab71c4695fa422723b591d747978d9accd
leonardofarinatti <127427288+leonardofarinatti@users.noreply.github.com>
2023-03-16 17:44:21 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -16,5 +16,41 @@ plot(l2011)
 cl <- colorRampPalette(c("red", "orange", "yellow")) (100)
 plot(l2011, col=cl) # per assegnare i nuovi colori alle immagini
 
-#plotting one element
+# plotting one element
 plot(l2011[[4]], col=cl)
+
+# Change the colour gamut for all images
+cl <- colorRampPalette(c("blue", "brown", "chartreuse", "chocolate", "darkorange")) (100)
+plot(l2011, col=cl)
+cl <- colorRampPalette(c("cyan", "azure", "darkorchid", "aquamarine")) (100)
+plot(l2011, col=cl)
+
+# Plot the NIR band
+plot(l2011[[4]], col=cl)
+
+# dev.off() it closes graph
+
+# Export graphs in R
+pdf("myfirstgraph.pdf")
+plot(l2011[[4]], col=cl)
+dev.off()
+
+# Plotting several bands in a multiframe
+par(mfrow=c(2,1))
+plot(l2011[[3]], col=cl)
+plot(l2011[[4]], col=cl)
+
+# Plotting the first 4 bands
+par(mfrow=c(2,2))
+# Blue
+clb <- colorRampPalette(c("blue4", "blue2", "light blue")) (100)
+plot(l2011[[1]], col=clb)
+# Green
+clg <- colorRampPalette(c("chartreuse4", "chartreuse2", "chartreuse")) (100)
+plot(l2011[[2]], col=clg)
+# Red
+clr <- colorRampPalette(c("brown4", "brown2", "brown")) (100)
+plot(l2011[[3]], col=clr)
+# NIR
+cln <- colorRampPalette(c("darkgoldenrod4", "darkgoldenrod2", "darkgoldenrod")) (100)
+plot(l2011[[4]], col=cln)
