7e15c973cd08d70ecf745f0c1226e235c8b8bb16
leonardofarinatti <127427288+leonardofarinatti@users.noreply.github.com>
2023-04-13 17:07:00 +0200

    Update time _series.R

--- time _series.R
+++ time _series.R
@@ -25,3 +25,37 @@ plot(dift)
 # levelplot(TGr)
 cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
 plot(TGr, col=cl)
+
+
+# Example 2: NO2 decrease during the lockdown period
+
+library(raster)
+setwd("C:/lab/en")
+# Importing a file
+en_first <- raster("EN_0001.png")
+rimp <- lapply(rlist, raster)
+cl <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(en_first, col=cl)
+# Let's import the whole set
+rlist <- list.files(pattern="EN")
+import <- lapply(rlist, raster)
+EN <- stack(import)
+plot(EN, col=cl)
+par(mfrow=c(1,2))
+plot(en_first, col=cl)
+plot(en[[1]], col=cl)
+# Check
+dif_check <- en_first - EN[[1]]
+dif_check
+plot(dif_check)
+# first and last data
+par(mfrow=c(1,2))
+plot(en[[1]], col=cl)
+plot(en[[13]], col=cl)
+# let's make the difference
+difen =  en[[1]] - en[[13]]
+cldif <- colorRampPalette(c('blue','white','red'))(100)
+plot(difen, col=cldif)
+# plotRGB of three files together
+plotRGB(en, r=1, g=7, b=13, stretch="lin")
+plotRGB(en, r=1, g=7, b=13, stretch="hist")
