d4f7eaaf101960a3d02a04b896f43d93f45a029a
leonardofarinatti <127427288+leonardofarinatti@users.noreply.github.com>
2023-04-14 17:15:09 +0200

    Update R_code_classification.R

--- R_code_classification.R
+++ R_code_classification.R
@@ -4,3 +4,22 @@ library(raster)
 setwd("C:/lab/")
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 plotRGB(so, 1, 2, 3, stretch="lin")
+plotRGB(so, 1, 2, 3, stretch="hist")
+# Classifying the solar data
+# 1. Get all the single values 
+singlenr <- getValues(so)  # singlenr= single numbers
+# 2. Classify
+kcluster <- kmeans(singlenr, centers = 3)
+# 3. Set values to a raster on the basis of so
+soclass <- setValues(so[[1]], kcluster$cluster) # assign new values to a raster object
+cl <- colorRampPalette(c('yellow','black','red'))(100)
+plot(soclass, col=cl)
+# class 2: highest energy level
+# class 3: lowest energy level
+# class 1: medium energy level
+frequencies <- freq(soclass)
+tot = 2221440  # Tot pixel
+percentages = frequencies * 100 /  tot
+# class 2: highest energy level: 21.21993
+# class 3: lowest energy level: 37.33349
+# class 1: medium energy level: 41.44658
