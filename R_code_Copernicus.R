85c89db30f4756c90bc2ed12467a7ca43415df68
leonardofarinatti <127427288+leonardofarinatti@users.noreply.github.com>
2023-05-11 17:51:17 +0200

    Update R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -6,3 +6,6 @@ library(ggplot2)
 library(viridis)
 setwd("C:/Lab/")
 # dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc
+
+sc <- raster("c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
+plot(sc)
