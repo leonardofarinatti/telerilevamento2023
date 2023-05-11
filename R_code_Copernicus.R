e04f2072d647f0ad9d8adef9227cd9940550d665
leonardofarinatti <127427288+leonardofarinatti@users.noreply.github.com>
2023-05-11 17:48:43 +0200

    Create R_code_Copernicus.R

---
+++ R_code_Copernicus.R
@@ -0,0 +1,8 @@
+# R code downloading and visualizing Copernicus data
+
+library(raster)
+library(ncdf4)
+library(ggplot2)
+library(viridis)
+setwd("C:/Lab/")
+# dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc
