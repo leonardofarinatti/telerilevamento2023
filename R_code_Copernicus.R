0be48bcd515f1f2381cfe78df8f084bc95250a47
leonardofarinatti <127427288+leonardofarinatti@users.noreply.github.com>
2023-05-12 17:00:16 +0200

    Update R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -9,3 +9,7 @@ setwd("C:/Lab/")
 
 sc <- raster("c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
 plot(sc)
+scd <- as.data.frame(sc, xy=T)
+ggplot() +
+geom_raster(scd, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture)) +
+ggtitle("Soil Moisture for Copernicus")
