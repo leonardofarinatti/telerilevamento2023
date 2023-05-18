449b0f0341ce146da476b593617b74d046cb1bda
leonardofarinatti <127427288+leonardofarinatti@users.noreply.github.com>
2023-05-18 18:02:23 +0200

    Update R_code_sdm.R

--- R_code_sdm.R
+++ R_code_sdm.R
@@ -40,3 +40,28 @@ plot(preds$precipitation, col=cl)
 points(presences, pch=19)
 plot(preds$vegetation, col=cl)
 points(presences, pch=19)
+
+# model
+
+# set the data for the sdm
+datasdm <- sdmData(train=species, predictors=preds)
+# model # modello lineare generalizzato
+m1 <- sdm(Occurrence ~ elevation + precipitation + temperature + vegetation, data=datasdm, methods = "glm")
+# make the raster output layer
+p1 <- predict(m1, newdata=preds) 
+# plot the output
+plot(p1, col=cl)
+points(presences, pch=19)
+
+# add to the stack
+s1 <- stack(preds,p1)
+plot(s1, col=cl)
+names(s1) <- c('elevation', 'precipitation', 'temperature', 'vegetation', 'model')
+
+# Do you want to change names in the plot of the stack?
+# Here your are!:
+# choose a vector of names for the stack, looking at the previous graph, qhich are:
+names(s1) <- c('elevation', 'precipitation', 'temperature', 'vegetation', 'model')
+# and then replot!:
+plot(s1, col=cl)
+# you are done, with one line of code (as usual!)
