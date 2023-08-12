8fc47eaaddf1d9fd9f802d95d8fd59a864e4dbfc
leonardofarinatti <127427288+leonardofarinatti@users.noreply.github.com>
2023-08-12 16:09:51 +0200

    Create R_code_Exam.R

---
+++ R_code_Exam.R
@@ -0,0 +1,7 @@
+# This is the code for my Telerilevamento Geo-Ecologico exam
+# This code will focus to the loss of snow of the Taku glacier, in Alaska
+
+# The code will show the following ponits:
+# 1. pca calculation (principal component analysis)
+# 2. loss of snow cover calculation
+# 3. standard deviation calculation to show the most changed points
