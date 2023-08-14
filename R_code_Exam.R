972f91294c334950aa9b133a4345d7c12575b752
leonardofarinatti <127427288+leonardofarinatti@users.noreply.github.com>
2023-08-14 16:36:44 +0200

    Update R_code_Exam.R

--- R_code_Exam.R
+++ R_code_Exam.R
@@ -1,5 +1,5 @@
 # This is the code for my Telerilevamento Geo-Ecologico exam
-# This code will focus to the loss of snow of the Taku glacier, in Alaska
+# This code will focus to the loss of snow of the Mendenhall glacier, in Alaska
 
 # The code will show the following ponits:
 # 1. pca calculation (principal component analysis)
@@ -13,106 +13,106 @@ library (raster)
 library (rasterVis)
 # install.packages ("ggplot2") # to plot with ggplot
 library (ggplot2)
-# install.packages ("gridExtra") # to plot the ggplots together
-library (gridExtra)
 # install.packages ("viridis")
 library(viridis)
 # Now set the working directory
 setwd ("C:/lab/Esame")
 
-# We can call our objects, for this code we need two images of two different years (2014 - 2019) of Taku glacier
+# We can call our objects, for this code we need two images of two different years (2014 - 2019) of Mendenhall glacier
 # Assign to those images a name
-taku_2014 <- brick("takuglacierretreat_oli_2014232_lrg.jpg")
-taku_2019 <- brick("takuglacierretreat_oli_2019221_lrg.jpg")
+mendenhall_1984 <- brick("mendenhallglacier_tm5_1984230_lrg.jpg")
+mendenhall_2023 <- brick("mendenhallglacier_oli_2023209_lrg.jpg")
 
 # Call them to see the imformation inside
 
-taku_2014
+mendenhall_1984
 # class      : RasterBrick 
-# dimensions : 4542, 5031, 22850802, 3  (nrow, ncol, ncell, nlayers)
+# dimensions : 585, 877, 513045, 3  (nrow, ncol, ncell, nlayers)
 # resolution : 1, 1  (x, y)
-# extent     : 0, 5031, 0, 4542  (xmin, xmax, ymin, ymax)
+# extent     : 0, 877, 0, 585  (xmin, xmax, ymin, ymax)
 # crs        : NA 
-# source     : takuglacierretreat_oli_2014232_lrg.jpg 
-# names      : takuglacierretreat_oli_2014232_lrg_1, takuglacierretreat_oli_2014232_lrg_2, takuglacierretreat_oli_2014232_lrg_3
-
-taku_2019
+# source     : mendenhallglacier_tm5_1984230_lrg.jpg 
+# names      : mendenhallglacier_tm5_1984230_lrg_1, mendenhallglacier_tm5_1984230_lrg_2, mendenhallglacier_tm5_1984230_lrg_3
+mendenhall_2023
 # class      : RasterBrick 
-# dimensions : 4542, 5031, 22850802, 3  (nrow, ncol, ncell, nlayers)
+# dimensions : 585, 877, 513045, 3  (nrow, ncol, ncell, nlayers)
 # resolution : 1, 1  (x, y)
-# extent     : 0, 5031, 0, 4542  (xmin, xmax, ymin, ymax)
+# extent     : 0, 877, 0, 585  (xmin, xmax, ymin, ymax)
 # crs        : NA 
-# source     : takuglacierretreat_oli_2019221_lrg.jpg 
-# names      : takuglacierretreat_oli_2019221_lrg_1, takuglacierretreat_oli_2019221_lrg_2, takuglacierretreat_oli_2019221_lrg_3 
+# source     : mendenhallglacier_oli_2023209_lrg.jpg 
+# names      : mendenhallglacier_oli_2023209_lrg_1, mendenhallglacier_oli_2023209_lrg_2, mendenhallglacier_oli_2023209_lrg_3
 
 # Plot the 2 images to see the 3 bands 
-plot(taku_2014)
+plot(mendenhall_1984)
 dev.off()
-plot(taku_2019)
+plot(mendenhall_2023)
 dev.off()
 
 # Let's save this two plot
-# Taku 2014 components
-jpeg("taku_2014_components.jpg", 900, 900)
-plot(taku_2014)
+# Mendenhall 1984 components
+jpeg("mendenhall_1984_components.jpg", 900, 900)
+plot(mendenhall_1984)
 dev.off()
-jpeg("taku_2019_components.jpg", 900, 900)
-plot(taku_2019)
+jpeg("mendenhall_2023_components.jpg", 900, 900)
+plot(mendenhall_2023)
 dev.off()
 
-# Now to see the images with real color we need to use "ggplot2" and "gridExtra" packages functions
+# Now to see the images with real color we need to use "ggplot2"
 # Function "ggRGB" calcolate the composite raster, following the bands order "red, green, blue", using a linear stretch
-p1 <- plotRGB(taku_2014,r=1,g=2,b=3, stretch="lin")
-p2 <- plotRGB(taku_2019,r=1,g=2,b=3, stretch="lin")
+plotRGB(mendenhall_1984,r=1,g=2,b=3, stretch="lin")
+plotRGB(mendenhall_2023,r=1,g=2,b=3, stretch="lin")
 
 # Combine the two plots into one to compare them
 par(mfrow=c(1,2))
-plot(p1)
-plot(p2)
+plotRGB(mendenhall_1984,r=1,g=2,b=3, stretch="lin")
+plotRGB(mendenhall_2023,r=1,g=2,b=3, stretch="lin")
 
 
 # 1. PCA CALCULATION
-# Create a random number of samples from taku_2014 pixels (can't use directly the image from PCA)
-sample_2014 <- sampleRandom(taku_2014, 10000)
+# Create a random number of samples from mendenhall_1984 pixels (can't use directly the image from PCA)
+sample_1984 <- sampleRandom(mendenhall_1984, 10000)
 # Calculate PCA
-pca_2014 <- prcomp(sample_2014)
+pca_1984 <- prcomp(sample_1984)
 # Variance explained
-summary(pca_2014)
+summary(pca_1984)
 # Importance of components:
-#                             PC1      PC2     PC3
-# Standard deviation     135.4307 11.43075 5.71805
-# Proportion of Variance   0.9912  0.00706 0.00177
-# Cumulative Proportion    0.9912  0.99823 1.00000
-# The 99% of variance is explained from the first component
+#                             PC1     PC2      PC3
+# Standard deviation     105.6623 50.7469 25.86243
+# Proportion of Variance   0.7749  0.1787  0.04642
+# Cumulative Proportion    0.7749  0.9536  1.00000
+# The 77% of variance is explained from the first component
 
 # Now the second picture
-sample_2019 <- sampleRandom(taku_2019, 10000)
-pca_2019 <- prcomp(sample_2019)
-summary(pca_2019)
+sample_2023 <- sampleRandom(mendenhall_2023, 10000)
+pca_2023 <- prcomp(sample_2023)
+summary(pca_2023)
 # Importance of components:
-#                             PC1      PC2     PC3
-# Standard deviation     142.6362 11.87155 7.63723
-# Proportion of Variance   0.9903  0.00686 0.00284
-# Cumulative Proportion    0.9903  0.99716 1.00000
-# The 99% of variance is explained from the first component
-
+#                            PC1     PC2      PC3
+# Standard deviation     103.631 55.0884 29.26900
+# Proportion of Variance   0.734  0.2074  0.05855
+# Cumulative Proportion    0.734  0.9415  1.00000
+# The 73% of variance is explained from the first component
+ 
 # Principal components map
-pci_2014 <- predict(taku_2014, pca_2014, index=c(1:2))
-plot(pci_2014[[1]])
-pci_2019 <- predict(taku_2019, pca_2019, index=c(1:2))
-plot(pci_2019[[1]])
+pci_1984 <- predict(mendenhall_1984, pca_1984, index=c(1:2))
+plot(pci_1984[[1]])
+pci_2023 <- predict(mendenhall_2023, pca_2023, index=c(1:2))
+plot(pci_2023[[1]])
 
 # ggplot
-pcid_2014 <- as.data.frame(pci_2014[[1]], xy=T)
-pcid_2014
+pcid_1984 <- as.data.frame(pci_1984[[1]], xy=T)
+pcid_1984
 ggplot() +
-geom_raster(pcid_2014, mapping = aes(x=x, y=y, fill=PC1)) +
-scale_fill_viridis()
-pcid_2019 <- as.data.frame(pci_2019[[1]], xy=T)
-pcid_2019
+geom_raster(pcid_1984, mapping = aes(x=x, y=y, fill=PC1)) +
+scale_fill_viridis() +
+ggtitle("pci 1984")
+
+pcid_2023 <- as.data.frame(pci_2023[[1]], xy=T)
+pcid_2023
 ggplot() +
-geom_raster(pcid_2019, mapping = aes(x=x, y=y, fill=PC1)) +
-scale_fill_viridis()
+geom_raster(pcid_2023, mapping = aes(x=x, y=y, fill=PC1)) +
+scale_fill_viridis() +
+ggtitle("pci 2023")
 
 
 # 2. LOSS OF SNOW
